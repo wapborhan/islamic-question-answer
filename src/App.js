67ab839f8ec0d5ac76f8c4710767861412c829a0
Borhan Uddin <68281712+wapborhan@users.noreply.github.com>
2023-03-14 14:22:06 +0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,8 @@
 function App() {
   return (
     <div className="App">
-      <header className="App-header h-100 w-100">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+      <header className="App-header h-100 w-100 d-flex justify-content-center">
+        <div className="m-3 p-5">Loding...</div>
       </header>
     </div>
   );
