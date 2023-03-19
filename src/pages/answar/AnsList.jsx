b43b1b5a2a39f01f7ce0975f598fa98980dc7032
Borhan Uddin <68281712+wapborhan@users.noreply.github.com>
2023-03-19 17:56:14 +0600

    Design Changes

--- src/pages/answar/AnsList.jsx
+++ src/pages/answar/AnsList.jsx
@@ -1,20 +1,18 @@
 import React from "react";
+import QuesCard from "./QuesCard";
 
 const AnsList = () => {
   return (
     <div className="cat-scrl lg:h-[calc(100vh-190px)] xl-min:h-[calc(100vh-170px)] lg-min:bg-white lg-min:dark:bg-hadith-dark-card rounded-t-none rounded-b-xl w-full xss:mb-10 xss:rounded-xl xss:mt-4 xs:rounded-xl sm:rounded-xl  md:rounded-xl lg:rounded-xl scroll-mt-20">
-      <div
-        id="chapTitle"
-        class="undefined h-[11rem] sm-max:h-fit p-6 bg-white dark:bg-hadith-dark-card rounded-xl xs-max:py-4 xs-max:px-4 
-      lg:rounded-b-xl "
-      >
-        <div class="mt-7">
-          <span class="dark:text-book-detail-text">
-            সংকলক : শাইখ ইমামুল হুজ্জাহ আবু ‘আবদুল্লাহ মুহাম্মাদ বিন ইসমা’ঈল বিন
-            ইবরাহীম বিন মুগীরাহ্‌ আল বুখারী আল-জু’ফী। মোট হাদীস সংখ্যা : ৭৫৬৩
-            টি। প্রকাশনী : তাওহীদ পাবলিকেশন্স। মৌলিক হাদীস গ্রন্থ হিসাবে সহীহুল
-            বুখারী গ্রন্থটি হ
-          </span>
+      {/* <div class="w-full mb-4 mx-auto h-[1px] bg-[#F6F7F7] dark:bg-dark-cat-border"></div> */}
+
+      <div className="pb-6 xss:mr-0">
+        <div className="lg-min:pl-3 lg-min:bg-white lg-min:dark:bg-hadith-dark-card rounded-2xl w-full pb-5">
+          <QuesCard />
+          <QuesCard />
+          <QuesCard />
+          <QuesCard />
+          <QuesCard />
         </div>
       </div>
     </div>
